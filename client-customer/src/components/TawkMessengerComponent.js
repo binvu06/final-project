148841c6994104ae9a0aaec65c8e0527c8f7a43e
binvu0602 <[vulegiahoa07]>
2023-12-01 11:01:36 +0700

    Initial commit

---
+++ client-customer/src/components/TawkMessengerComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
+class TawkMessenger extends Component {
+   render() {
+      return (
+         <TawkMessengerReact
+            // propertyId="65671e3f26949f791135e1f2"
+            // widgetId="1hgdcsapl"
+            propertyId="65694e6bbfb79148e598c5ca"
+            widgetId="1hghlkluc"
+         />
+      );
+   }
+}
+export default TawkMessenger;
